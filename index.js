0860a44b1b2a1929db2b7b1201f4e9255d590daf
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-02-11 05:13:13 -0300

    display clear working, finishing calculations

--- index.js
+++ index.js
@@ -1,56 +1,26 @@
 "use strict";
-const outPut = document.querySelector('.number-output');
-let deleteBtn = document.querySelector('.deletebtn');
+let output = document.querySelector('.number-output');
+const deleteBtn = document.querySelector('.deletebtn');
 let clearBtn = document.querySelector('.clearbtn');
 let operandBtn = document.querySelector('.operand');
 const buttons = document.querySelectorAll('.number');
-let a = '';
-let b = '';
+let a = '0';
+let b = '0';
 let operator = '';
-buttons.forEach((button) => {
-    button.addEventListener('click', function () {
-        outPut.textContent += button.textContent;
-        let buttonText = this.innerText;
-    });
-});
-clearBtn === null || clearBtn === void 0 ? void 0 : clearBtn.addEventListener('click'), () => {
-    outPut === null || outPut === void 0 ? void 0 : outPut.textContent = '';
-    a = '';
-    b = '';
-    operator = '';
-    updateDisplayedValues();
-};
-deleteBtn === null || deleteBtn === void 0 ? void 0 : deleteBtn.addEventListener('click'), () => {
-    var _a;
-    (_a = outPut === null || outPut === void 0 ? void 0 : outPut.textContent) === null || _a === void 0 ? void 0 : _a.text.slice(0, -1);
-};
-const updateDisplayedValues = function () {
-    if (operands === "") {
-        outPut.textContent = a;
-        outPut.textContent = b;
-    }
-    outPut.textContent = `${a} ${operator}`;
-    outPut.textContent = b;
-};
-function add(a, b) { return a + b; }
-function divide(a, b) { return a / b; }
-function multiply(a, b) { return a * b; }
-function subtract(a, b) { return a - b; }
-function calculate(a, b, operator) {
-    switch (operator) {
-        case '+':
-            return add(a, b);
-            break;
-        case '-':
-            return subtract(a, b);
-            break;
-        case '*':
-            return multiply(a, b);
-            break;
-        case '/':
-            return divide(a, b);
-            break;
-        default:
-            return 0;
-    }
+//  buttons.forEach((button) => {
+//      button.addEventListener('click', function() {
+//           output.textContent += button.textContent
+//      });
+//  });
+function displayValue(input) {
+    output.textContent += input;
+}
+function clearDisplay() {
+    output.textContent = "";
+}
+function deleteDisplay() {
+    output.innerText.slice(0, -1);
+}
+function calculate() {
+    output.textContent = eval(output.value);
 }
--- index.ts
+++ index.ts
@@ -1,70 +1,37 @@
-const outPut = document.querySelector('.number-output')
-let deleteBtn = document.querySelector('.deletebtn')
+let output = <HTMLOutputElement> document.querySelector('.number-output')
+const deleteBtn = <HTMLButtonElement>document.querySelector('.deletebtn')
 let clearBtn = document.querySelector('.clearbtn')
 let operandBtn = document.querySelector('.operand')
 const buttons = document.querySelectorAll('.number');
 
-let a = ''
-let b = ''
+let a = '0'
+let b = '0'
 let operator = ''
 
 
-buttons.forEach((button) => {
-    button.addEventListener('click', function() {
-        outPut.textContent += button.textContent
-        
-        
-         let buttonText = this.innerText
-    });
-});
 
-clearBtn?.addEventListener('click'), () => {
-    outPut?.textContent = ''
-    a = ''
-    b = ''
-    operator = ''
-    updateDisplayedValues();
+//  buttons.forEach((button) => {
+//      button.addEventListener('click', function() {
+//           output.textContent += button.textContent
+//      });
+//  });
+
+function displayValue(input) {
+    output.textContent += input;
+}
+    
+function clearDisplay() {
+    output.textContent = "";
  } 
 
 
-deleteBtn?.addEventListener('click'), () => {
-         outPut?.textContent?.text.slice(0,-1)
+function deleteDisplay() {
+    output.innerText.slice(0,-1)
+}
+function calculate() {
+    output.textContent = eval(output.value)
 }
 
-
-const updateDisplayedValues = function () {
-    if (operands === "") {
-      outPut.textContent = a;
-      outPut.textContent = b;
-    }
-    outPut.textContent = `${a} ${operator}`;
-    outPut.textContent = b;
-  };
-
-
-
-function add(a, b) {return a + b}
-function divide(a, b) {return a / b}
-function multiply(a, b) {return a * b}
-function subtract(a, b) {return a - b}
-function calculate(a, b, operator) {
-    switch (operator) {
-        case '+':
-            return add(a, b)
-            break;
-        case '-':
-            return subtract(a, b)
-            break;
-        case '*':
-            return multiply(a, b)
-            break;
-        case '/':
-            return divide(a, b)
-            break;
-        default:
-            return 0;
-    }
-}
 
 
 
